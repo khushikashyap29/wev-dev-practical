1181d8b4538d17e3094bd15ea48de3cd893997b6
khushikashyap29 <khushikashyap3feb2007@gmail.com>
2026-05-04 10:32:04 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div style={{ textAlign: "centre", marginTop: "50px" }}>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
